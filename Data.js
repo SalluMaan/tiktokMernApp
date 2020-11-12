9bd48e0ed824c71ef1a80a590333768279827f07
SalluMaan <maansallu786@gmail.com>
2020-11-12 11:41:27 +0500

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,24 @@
+const Data = [
+  {
+    url:
+      "https://v77.tiktokcdn.com/834bc08c2a7ea17d6326b24067acf0ba/5fac6882/video/tos/alisg/tos-alisg-pve-0037c001/e82cabb96b71415982bb9186e5068133/?a=1233&br=5372&bt=2686&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202011111640510102340870133473856A&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M3lobW00b3k0eDMzOjczM0ApPGk7NDg1aTtkNzs7MztmOGdtaWhyYmJnMzNfLS0zMTRzcy9iY2FgYjIxY2IyYzE2LmA6Yw%3D%3D&vl=&vr=",
+    channel: "MaanSallu321",
+    song: "Let's Play Game",
+    likes: "3333",
+    messages: "21",
+    description: "This is new Description outdated.",
+    shares: "5",
+  },
+  {
+    url:
+      "https://v77.tiktokcdn.com/765c59b2f6b041b962db5c786dadaa2b/5fac72ab/video/tos/useast2a/tos-useast2a-ve-0068c004/3376b17831094ba39c09b58025fe3ce1/?a=1233&br=2168&bt=1084&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2020111117241501011507907017769DD6&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajdxM3h1NnQ0dzMzNzczM0ApNTM5aDZoaGRpNzk1O2k1aGdkYGc1c2ZwaV5fLS0yMTZzczAuNGJfNWAvYWNiYDRjNi46Yw%3D%3D&vl=&vr=",
+    channel: "Anmaan786",
+    song: "Let's Play Song of Love",
+    likes: "765",
+    messages: "65",
+    description: "This is new Description Updated.",
+    shares: "2",
+  },
+];
+
+module.exports = Data;
---
+++ server.js
@@ -0,0 +1,60 @@
+// ---- App Config
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const Data = require("./data");
+const Video = require("./dbModel");
+// const morgan = require("morgan");
+// const expressValidator = require("express-validator");
+// var cookieParser = require("cookie-parser");
+// const bodyparser = require("body-parser");
+
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeaders("Access-Control-Allow-Origin", "*");
+  res.setHeaders("Access-Control-Allow-Headers", "*");
+  next();
+});
+// ---- Database Config
+const UrlMongoDB =
+  "mongodb+srv://admin:Salman123@cluster0.dybdg.mongodb.net/<tiktokClone>?retryWrites=true&w=majority";
+mongoose
+  .connect(UrlMongoDB, {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true,
+  })
+  .then((d) => console.log("db Connected"))
+  .catch((err) => {
+    console.log(`DB Connection Error ${err.message}`);
+  });
+
+// console.log(Data);
+// ---- Api EndPoints
+app.get("/", (req, res) => res.status(200).json(Data));
+app.post("/addpost", (req, res) => {
+  const dbVideos = req.body;
+  Video.create(dbVideos, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/allposts", (req, res) => {
+  Video.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+const port = 9000;
+app.listen(port, () => {
+  console.log("A Node JS API is listening on PORT:", port);
+});
